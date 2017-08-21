b171c446a7807f132329733522290758a144e899
Jerry Lee <leejb91@gmail.com>
2017-08-20 22:06:40 -0700

    change to .env file

--- gohome/controller/googleMapsAPI.js
+++ gohome/controller/googleMapsAPI.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
+
+var APIKEY = process.env.APIKEY;
+var directionsUrl = 'https://maps.googleapis.com/maps/api/directions/';
+
 function hello(req, res) {
-	res.json({message: 'hello'})
+	res.json({message: APIKEY})
 }
 
 
